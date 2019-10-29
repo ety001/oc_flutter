af38e3e9eac258e785aa1f1437032ed9ba7f5602
ETY001 <ety001@domyself.me>
2019-10-30 03:35:17 +0800

    main page ui finish

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Online Clipboard',
+    title: '网络剪切板',
     home: Home(),
   ));
 }
@@ -12,17 +12,47 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Online Clipboard'),
+        title: Text('网络剪切板'),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ClipContent()),
-            );
-          },
+        child: Padding(
+          padding: EdgeInsets.only(top: 100.0),
+          child: Column(
+            children: <Widget>[
+              Text('多终端同步文字信息, 最多50条'),
+              Text('输入剪切板名称: public，密码: public，可以进行留言'),
+              Padding(
+                padding: EdgeInsets.only(top: 50.0, left: 40.0, right: 40.0),
+                child: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: '剪切板名称',
+                  ),
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 30.0, left: 40.0, right: 40.0),
+                child: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: '剪切板密码',
+                  ),
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 70.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  colorBrightness: Brightness.light,
+                  textTheme: ButtonTextTheme.primary,
+                  child: const Text(
+                    '开始使用',
+                    style: TextStyle(fontSize: 20)
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
