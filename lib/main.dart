fcf3c5d577a78b55c364351d9cb53a8453186e42
ETY001 <ety001@domyself.me>
2019-10-30 15:40:03 +0800

    change home page ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,26 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('网络剪切板'),
-      ),
       body: Center(
         child: Padding(
           padding: EdgeInsets.only(top: 80.0),
           child: Column(
             children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 40.0),
+                child:   Text(
+                  '网络剪切板',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
               Text('多终端同步文字信息, 最多50条'),
               Text('输入剪切板名称: public，密码: public，可以进行留言'),
               Padding(
                 padding: EdgeInsets.only(top: 30.0, left: 40.0, right: 40.0),
                 child: TextField(
-                  autofocus: true,
                   decoration: InputDecoration(
                     labelText: '剪切板名称',
                   ),
